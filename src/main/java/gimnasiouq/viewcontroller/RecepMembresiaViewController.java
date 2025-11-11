e4978bcca7a7aa0bc677e66968f961733b6e0a91
Isi578 <isabela.quinterom@uqvirtual.edu.co>
2025-11-11 12:02:50 -0500

    Implementar el enlace de datos para la vista de tabla de membresía
    Se agregó el enlace de datos y el detector de selección para la tabla de usuarios en RecepMembresiaViewController. Se actualizó FXML para asignar fx:id a TableView y así lograr una interacción adecuada con el controlador. Esto mejora la capacidad de respuesta de la interfaz de usuario y permite mostrar los detalles de membresía de los usuarios.

--- src/main/java/gimnasiouq/viewcontroller/RecepMembresiaViewController.java
+++ src/main/java/gimnasiouq/viewcontroller/RecepMembresiaViewController.java
@@ -1,13 +1,23 @@
 package gimnasiouq.viewcontroller;
 
+import gimnasiouq.factory.ModelFactory;
+import gimnasiouq.model.Estudiante;
+import gimnasiouq.model.Trabajador;
+import gimnasiouq.model.Usuario;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class RecepMembresiaViewController {
+
+    ObservableList<Usuario> listaUsuarios;
+    Usuario usuarioSeleccionado;
 
     @FXML
     private Button btnActualizar;
@@ -16,64 +26,150 @@ public class RecepMembresiaViewController {
     private Button btnAsignar;
 
     @FXML
-    private Button btnDetalles;
-
-    @FXML
     private Button btnEliminar;
 
     @FXML
     private Button btnNuevo;
 
     @FXML
-    private ComboBox<?> comboBoxPlanMembresia;
+    private ComboBox<String> comboBoxPlanMembresia;
 
     @FXML
-    private TableColumn<?, ?> tcCosto;
+    private TableView<Usuario> tableView;
 
     @FXML
-    private TableColumn<?, ?> tcEstado;
+    private TableColumn<Usuario, String> tcCosto;
 
     @FXML
-    private TableColumn<?, ?> tcFechaFin;
+    private TableColumn<Usuario, String> tcEstado;
 
     @FXML
-    private TableColumn<?, ?> tcFechaInicio;
+    private TableColumn<Usuario, String> tcFechaFin;
 
     @FXML
-    private TableColumn<?, ?> tcIdentificacion;
+    private TableColumn<Usuario, String> tcFechaInicio;
 
     @FXML
-    private TableColumn<?, ?> tcNombre;
+    private TableColumn<Usuario, String> tcIdentificacion;
 
     @FXML
-    private TableColumn<?, ?> tcPlan;
+    private TableColumn<Usuario, String> tcNombre;
 
     @FXML
-    private TableColumn<?, ?> tcUsuario;
+    private TableColumn<Usuario, String> tcPlan;
 
     @FXML
-    void onActualizar(ActionEvent event) {
+    private TableColumn<Usuario, String> tcUsuario;
+
+    @FXML
+    private TextField txtCosto;
+
+    @FXML
+    private TextField txtFechaFin;
+
+    @FXML
+    private TextField txtFechaInicio;
+
+    @FXML
+    void onActualizar(ActionEvent event) {actualizarMembresia();}
+
+    private void actualizarMembresia() {
 
     }
 
     @FXML
-    void onAsignar(ActionEvent event) {
+    void onAsignar(ActionEvent event) {asignarMembresia();}
+
+    private void asignarMembresia() {
 
     }
 
     @FXML
-    void onDellates(ActionEvent event) {
+    void onEliminar(ActionEvent event) {eliminarMembresia();}
+
+    private void eliminarMembresia() {
 
     }
 
     @FXML
-    void onEliminar(ActionEvent event) {
+    void onNuevo(ActionEvent event) {nuevaMembresia();}
+
+    private void nuevaMembresia() {
 
     }
 
     @FXML
-    void onNuevo(ActionEvent event) {
+    void initialize() {
+        initView();
+        comboBoxPlanMembresia.getItems().addAll("Mensual", "Trimestral", "Anual");
+    }
 
+    private void initView() {
+        initDataBinding();
+        listaUsuarios = ModelFactory.getInstance().obtenerUsuariosObservable();
+        tableView.setItems(listaUsuarios);
+        listenerSelection();
+    }
+
+    private void initDataBinding() {
+        if (tcNombre != null) {
+            tcNombre.setCellValueFactory(cellData ->
+                    new SimpleStringProperty(cellData.getValue().getNombre()));
+        }
+        if (tcIdentificacion != null) {
+            tcIdentificacion.setCellValueFactory(cellData ->
+                    new SimpleStringProperty(cellData.getValue().getIdentificacion()));
+        }
+        if (tcFechaInicio != null) {
+            tcFechaInicio.setCellValueFactory(cellData -> {
+                Usuario usuario = cellData.getValue();
+                return new SimpleStringProperty(usuario.getFechaInicioFormateada());
+            });
+        }
+        if (tcFechaFin != null) {
+            tcFechaFin.setCellValueFactory(cellData -> {
+                Usuario usuario = cellData.getValue();
+                return new SimpleStringProperty(usuario.getFechaFinFormateada());
+            });
+        }
+        if (tcPlan != null) {
+            tcPlan.setCellValueFactory(cellData -> {
+                Usuario usuario = cellData.getValue();
+                return new SimpleStringProperty(usuario.getPlanMembresia());
+            });
+        }
+        if (tcCosto != null) {
+            tcCosto.setCellValueFactory(cellData -> {
+                Usuario usuario = cellData.getValue();
+                return new SimpleStringProperty(usuario.getCostoMembresiaFormateado());
+            });
+        }
+        if (tcEstado != null) {
+            tcEstado.setCellValueFactory(cellData ->
+                    new SimpleStringProperty(cellData.getValue().getEstadoMembresia()));
+        }
+        if (tcUsuario != null) {
+            tcUsuario.setCellValueFactory(cellData -> {
+                if (cellData.getValue() instanceof Estudiante) {
+                    return new SimpleStringProperty("Estudiante");
+                } else if (cellData.getValue() instanceof Trabajador) {
+                    return new SimpleStringProperty("Trabajador UQ");
+                } else {
+                    return new SimpleStringProperty("Externo");
+                }
+            });
+        }
+    }
+
+    private void listenerSelection() {
+        tableView.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newSelection) -> {
+                    usuarioSeleccionado = newSelection;
+                    mostrarInformacionUsuario(usuarioSeleccionado);
+                });
+    }
+
+    private void mostrarInformacionUsuario(Usuario usuarioSeleccionado) {
     }
 
 }
